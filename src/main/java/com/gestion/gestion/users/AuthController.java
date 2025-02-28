ccd09f58abe0565153b5f5f8a0e293ffa7efb2d6
eroudini <eroudini.abdillah@yahoo.com>
2025-02-28 12:18:08 +0100

    ajoute de la route connexion

--- src/main/java/com/gestion/gestion/users/AuthController.java
+++ src/main/java/com/gestion/gestion/users/AuthController.java
@@ -1,6 +1,13 @@
 package com.gestion.gestion.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +20,32 @@ public class AuthController {
     @Autowired
     private UserService userService;
 
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public AuthController(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        this.authenticationManager = authenticationConfiguration.getAuthenticationManager();
+    }
+
     @PostMapping("/register")
     public User registerUser(@RequestBody User user){
         return userService.registerUser(user);
+    }
+
+    @PostMapping("/login")
+    public String loginUser(@RequestBody User user) {
+        try {
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
+
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            return "Connexion réussie";
+        } catch (BadCredentialsException e) {
+            return "Identifiants incorrects";
+        }
     }
 
 
